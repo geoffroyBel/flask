69f09fe1e28068e5f9b0e7246f1bf9dc9337d41e
geoffroyBel <rastakongoner@gmail.com>
2023-06-04 12:54:49 +0200

    Flask boot app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-reports = [{"name": "My Store", "items": [{"name": "Chair", "price": 15.99}]}]
+reports = [{"name": "My reports", "items": [{"name": "Chair", "price": 15.99}]}]
 
 @app.get("/reports")
 def get_reports():
